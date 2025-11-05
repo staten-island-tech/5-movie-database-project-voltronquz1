d00c98507fa4ce86b7c8a1391748ecd570a8e886
a <justinc1127@nycstudents.net>
2025-11-05 10:17:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
+for index, item in enumerate(data):
+    print(index, ":", item["title"])
+year=input("Give me a movie in that database ")
+
