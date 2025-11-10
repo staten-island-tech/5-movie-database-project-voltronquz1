f969f5ecbbad2cc664b2ec5674dc7948b3a231fc
a <justinc1127@nycstudents.net>
2025-11-10 09:31:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,21 +6,27 @@ data = json.load(movies)
 
 # for index, item in enumerate(data):
 #     print(index, ':', item["title"])
-choice=int(input("Give me a movie year in that database "))
+# choice=int(input("Give me a movie year in that database "))
 # file two
 # for i in data:
 #     if i['year'] > choice:
 #         print(i['title']) 
 
 # file 3
-for i in data:
-    if i['year'] > choice < i['year']:
-        print(i["title"])
+# for i in data:
+#     if i['year'] > choice < i['year']:
+#         print(i["title"])
 # file 4
 # for i in data:
 #     if i['year']==choice:
 #         print(i['title'])
-# choice=input("Give me a word to look for movie with that word")
-# # file 5
-# for i in data:
-#     if i['title']==choice:
+choice=input("Give me a word to look for movie with that word: ")
+# file 5
+counter=0
+for i in data:
+    if  choice.lower() in i['title'].lower():
+        print(i['title'])
+        counter+=1
+    if counter <0:
+        print("There is nothing with that keyword give me something else")
+        print("")
